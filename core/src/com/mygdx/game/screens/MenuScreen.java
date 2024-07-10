69cccc5db327dd31028a6986c1e1491c30f4df57
Apolona <122671800+Apolona@users.noreply.github.com>
2024-07-10 12:56:16 +0300

    add new texture for menu screen

--- core/src/com/mygdx/game/screens/MenuScreen.java
+++ core/src/com/mygdx/game/screens/MenuScreen.java
@@ -17,15 +17,17 @@ public class MenuScreen extends ScreenAdapter {
     Button playButton;
     Button settingsButton;
     Button exitButton;
+    Button leaderButton;
     MovingBackground movingBackground;
     // jumpButton = new Button(GameResources.JUMP_BUTTON, 700, -300, (int) (200 * GameSettings.OBJECT_SCALE));
     public MenuScreen (MyGdxGame myGdxGame){
         this.myGdxGame = myGdxGame;
-        playButton = new Button(GameResources.MENU_BUTTON, 0, 0, 600*GameSettings.OBJECT_SCALE, 85*GameSettings.OBJECT_SCALE, myGdxGame.bitmapFont, "play");
-        settingsButton = new Button(GameResources.MENU_BUTTON, 0, -100, 600*GameSettings.OBJECT_SCALE, 85*GameSettings.OBJECT_SCALE, myGdxGame.bitmapFont, "settings");
-        exitButton = new Button(GameResources.MENU_BUTTON, 0, -200, 600*GameSettings.OBJECT_SCALE, 85*GameSettings.OBJECT_SCALE,myGdxGame.bitmapFont, "exit");
+        playButton = new Button(GameResources.MENU_BUTTON, 0, 0, 650*GameSettings.OBJECT_SCALE, 85*GameSettings.OBJECT_SCALE, myGdxGame.bitmapFont, "play");
+        leaderButton = new Button(GameResources.MENU_BUTTON, 0, -100, 650*GameSettings.OBJECT_SCALE, 85*GameSettings.OBJECT_SCALE, myGdxGame.bitmapFont, "leaders");
+        settingsButton = new Button(GameResources.MENU_BUTTON, 0, -200, 650*GameSettings.OBJECT_SCALE, 85*GameSettings.OBJECT_SCALE, myGdxGame.bitmapFont, "settings");
+        exitButton = new Button(GameResources.MENU_BUTTON, 0, -300, 650*GameSettings.OBJECT_SCALE, 85*GameSettings.OBJECT_SCALE,myGdxGame.bitmapFont, "exit");
 
-        movingBackground = new MovingBackground(GameResources.SKY);
+        movingBackground = new MovingBackground(GameResources.MENU_SCREEN);
 
 
 
@@ -39,6 +41,7 @@ public class MenuScreen extends ScreenAdapter {
 
         movingBackground.draw(myGdxGame.batch, myGdxGame);
         playButton.draw(myGdxGame.batch, myGdxGame.camera.position);
+        leaderButton.draw(myGdxGame.batch, myGdxGame.camera.position);
         settingsButton.draw(myGdxGame.batch, myGdxGame.camera.position);
         exitButton.draw(myGdxGame.batch, myGdxGame.camera.position);
 
@@ -48,6 +51,9 @@ public class MenuScreen extends ScreenAdapter {
         if (Gdx.input.isTouched()) {
             if(myGdxGame.gameScreen.buttonHandler(playButton)){
                 myGdxGame.setScreen(myGdxGame.gameScreen);
+            }
+            else if(myGdxGame.gameScreen.buttonHandler(leaderButton)){
+                //myGdxGame.setScreen(myGdxGame.);
             }
             else if(myGdxGame.gameScreen.buttonHandler(settingsButton)){
                 myGdxGame.setScreen(myGdxGame.settingsScreen);
