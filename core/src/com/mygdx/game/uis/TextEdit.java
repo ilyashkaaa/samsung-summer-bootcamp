2a5a2f359cf82b5a6b4c725527d188c09996a79c
Student <kriperguy9881@mail.ru>
2024-07-11 12:08:49 +0300

    added max length for name

--- core/src/com/mygdx/game/uis/TextEdit.java
+++ core/src/com/mygdx/game/uis/TextEdit.java
@@ -7,7 +7,10 @@ import com.mygdx.game.MyGdxGame;
 public class TextEdit implements Input.TextInputListener {
     @Override
     public void input(String text) {
-        MyGdxGame.name = text;
+        MyGdxGame.name = "";
+        for (int i = 0; i < Math.min(10, text.length()); i++) {
+            MyGdxGame.name += text.toCharArray()[i];
+        }
         MemoryManager.saveName(text);
     }
 
